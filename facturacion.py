48fa41a0506e08a715347456d3ab40cf66076965
Antonio <trujillohernandezraquel@outlook.es>
2026-05-17 01:10:53 +0100

    Fix: Limpieza final en facturación y actualización de ruta de guardado hacia OneDrive

--- facturacion.py
+++ facturacion.py
@@ -390,8 +390,7 @@ def render_pestana_facturacion(client):
                                         
                                 # Archivo Fiscal Físico (Guardar foto en local)
                                 try:
-                                    carpeta_facturas = os.path.join("Facturas_Guardadas", str(datetime.now().year), f"{datetime.now().month:02d}")
-                                    RUTA_BASE_FACTURAS = r"D:\animalarium\Mis facturas digitales"
+                                    RUTA_BASE_FACTURAS = r"C:\Users\truji\OneDrive\Documentos\ANIMALARIUM\TPV ANIMALARIUM\CONTABILIDAD\Mis facturas digitales"
                                     carpeta_facturas = os.path.join(RUTA_BASE_FACTURAS, str(datetime.now().year), f"{datetime.now().month:02d}")
                                     os.makedirs(carpeta_facturas, exist_ok=True)
                                     n_prov_archivo = datos_ia.get("nombre_proveedor", "Acreedor").replace(" ", "_").replace("/", "-")
